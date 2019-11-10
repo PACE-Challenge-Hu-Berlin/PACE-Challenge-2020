9823c37228df7edbd8e2afadbd170d67a6f33501
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-11-10 19:55:50 +0100

    main: Add --solver option to select algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,62 @@
+#include <cstring>
 #include <fstream>
 
 #include "io.hpp"
 #include "naive-branching-solver.hpp"
+#include "simple-pid-solver.hpp"
 
 static const char *usage_text =
 	"Usage: td [OPTIONS] INSTANCE\n"
 	"Possible OPTIONS are:\n"
-	"    None\n";
+	"    --solver SOLVER\n"
+	"        Select a solver algorithm {naive-branching, simple-pid}\n";
+
+enum class solver_algorithm {
+	none,
+	naive_branching,
+	simple_pid,
+};
 
 struct options {
 	const char *instance = nullptr;
+	solver_algorithm solver = solver_algorithm::naive_branching;
 };
 
 void parse_options(char **p, options &opts) {
 	auto error = [] (const char *text) {
-		std::cerr << "Usage error: " << text << "\n" << usage_text << std::flush;
+		std::cerr << usage_text << "Usage error: " << text << std::endl;
 		exit(2);
 	};
 
+	// Argument for unary options.
+	const char *arg;
+
+	auto handle_unary_option = [&] (const char *name) -> bool {
+		assert(*p);
+		if(std::strcmp(*p, name))
+			return false;
+		++p;
+		if(!(*p))
+			error("expected argument for unary option");
+		arg = *p;
+		++p;
+		return true;
+	};
+
 	// TODO: Here, we can parse command line arguments.
+	while(*p && !std::strncmp(*p, "--", 2)) {
+		if(handle_unary_option("--solver")) {
+			if(!std::strcmp(arg, "naive-branching")) {
+				opts.solver = solver_algorithm::naive_branching;
+			}else if(!std::strcmp(arg, "simple-pid")) {
+				opts.solver = solver_algorithm::simple_pid;
+			}else{
+				error("unknown solver algorithm");
+			}
+		}else{
+			error("unknown command line option");
+		}
+	}
 
 	if(!(*p))
 		error("not enough positional arguments");
@@ -40,7 +78,14 @@ int main(int argc, char *argv[]) {
 	}
 	std::cerr << "graph has " << g.num_vertices() << " vertices" << std::endl;
 
-	naive_branching_solver solver{g};
-	int solution = solver.compute_treedepth();
+	int solution;
+	if(opts.solver == solver_algorithm::naive_branching) {
+		naive_branching_solver solver{g};
+		solution = solver.compute_treedepth();
+	}else{
+		assert(opts.solver == solver_algorithm::simple_pid);
+		simple_pid_solver solver{g};
+		solution = solver.compute_treedepth();
+	}
 	std::cout << "s " << solution << std::endl;
 }
